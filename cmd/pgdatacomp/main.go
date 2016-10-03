8b13a2da0d5364619b0f12b8b943570ec69e8335
genez <lini.genesio@gmail.com>
2016-10-03 14:19:55 +0200

    better now thanks to gopher Sean Kelly

--- cmd/pgdatacomp/main.go
+++ cmd/pgdatacomp/main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
+	"encoding/csv"
+	"flag"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
 	"os"
+	"strconv"
 	"sync"
+	"time"
 )
 
 var wg sync.WaitGroup
 
 func main() {
+	vacuum := flag.Bool("vacuum", false, "Performs a VACUUM ANALYZE before starting")
+	flag.Parse()
 
 	db, err := sqlx.Open("postgres", "postgres://postgres:postgres@192.168.0.2/zucchetti?sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	if *vacuum {
+		log.Println("Performing VACUUM ANALYZE...")
+		db.MustExec("VACUUM ANALYZE;")
+		log.Println("...done")
+	}
 
 	rows, err := db.Queryx("SELECT relname FROM pg_stat_user_tables where  n_live_tup between 1 and 500 ORDER BY n_live_tup DESC;")
 	if err != nil {
@@ -51,13 +61,16 @@ func main() {
 }
 
 func dumpTable(db *sqlx.DB, tableName string) {
-	f, err := os.Create(tableName + ".sql")
+	defer wg.Done()
+
+	f, err := os.Create(tableName + ".csv")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
-	w := bufio.NewWriter(f)
+	w := csv.NewWriter(f)
+	defer w.Flush()
 
 	rows, err := db.Queryx("SELECT * FROM " + tableName)
 	if err != nil {
@@ -65,17 +78,45 @@ func dumpTable(db *sqlx.DB, tableName string) {
 	}
 	defer rows.Close()
 
-	results := make(map[string]interface{})
+	columns, err := rows.Columns()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write(columns)
+
 	for rows.Next() {
-		rows.MapScan(results)
-		jsonString, err := json.MarshalIndent(results, "", "   ")
+		results, err := rows.SliceScan()
 		if err != nil {
 			log.Fatal(err)
 		}
-		w.WriteString(string(jsonString))
+
+		s := make([]string, len(results))
+		for i, x := range results {
+			s[i] = ColumnToString(x)
+		}
+		w.Write(s)
 	}
+}
 
-	w.Flush()
-
-	wg.Done()
+func ColumnToString(col interface{}) string {
+	switch col.(type) {
+	case float64:
+		return strconv.FormatFloat(col.(float64), 'f', 6, 64)
+	case int64:
+		return strconv.FormatInt(col.(int64), 10)
+	case bool:
+		return strconv.FormatBool(col.(bool))
+	case []byte:
+		return string(col.([]byte))
+	case string:
+		return col.(string)
+	case time.Time:
+		return col.(time.Time).String()
+	case nil:
+		return "NULL"
+	default:
+		// Need to handle anything that ends up here
+		return "ERROR"
+	}
 }
